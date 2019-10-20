8d7736c016bc1571e4d985ccf75a63fe8af61840
HyeonminPark <hyeonminpark1306@gmail.com>
2019-10-20 13:39:50 -0400

    learning_log project start

---
+++ LearningLog/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USER\Desktop\Python\Python-Django\LearningLog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
